381600dc69f66165b208a05717312f6b284b5776
johnromanoff <johnromanoff@yandex.ru>
2019-01-28 02:48:22 +0300

    update

---
+++ src/game_geek/schema.clj
@@ -0,0 +1,19 @@
+(ns game-geek.schema 
+  "Contains custom resolvers and function provide the full schema."
+  (:require
+    [clojure.java.io :as io]
+    [com.walmartlabs.lacinia.util :as util]
+    [com.walmartlabs.lacinia.schema :as schema]
+    [clojure.edn :as edn]))
+
+(defn resolver-map 
+  []
+  {:query/game-by-id (fn [context args value] nil)})
+
+(defn load-schema 
+  []
+  (-> (io/resource "cgg-schema.edn")
+      slurp 
+      edn/read-string
+      (util/attach-resolvers (resolver-map)) 
+      schema/compile))
